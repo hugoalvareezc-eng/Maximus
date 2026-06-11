23560bea3165e84d89a8f235885a17b70013dc52
hugoalvareezc-eng <hugoalvareezc@gmail.com>
2026-06-10 20:16:53 -0600

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -2,7 +2,7 @@ import sys
 import os
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import gimnasio_crud as db
-from datetime import datetime
+from datetime import datetime, timedelta
 import locale
 
 # --- Configuración Inicial ---
